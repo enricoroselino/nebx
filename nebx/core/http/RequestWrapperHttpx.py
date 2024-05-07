ce84575c45e57d8aeeb1264f1ffb41ea8780f0a4
enricoroselino <enricoroselino@gmail.com>
2024-05-07 09:55:30 +0700

    feat(code): added IRequestWrapper and RequestWrapperHttpx
    - added RequestWrapperHttpx to reduce the "using with" statement on opening http client and response
    - added IRequestWrapper for its interface

---
+++ nebx/core/http/RequestWrapperHttpx.py
@@ -0,0 +1,52 @@
+﻿import asyncio
+import httpx
+
+from nebx.interfaces import IRequestWrapper
+from nebx.types import HTTPResponse
+
+
+class RequestWrapperHttpx(IRequestWrapper):
+    def __init__(self, proxy: str | None = None, ssl_strict: bool = True):
+        self.__ssl_strict = ssl_strict
+        self.__proxy = proxy
+        self.__client_session = self.__create_client()
+
+    async def get(self, url: str, headers: dict | None = None) -> HTTPResponse:
+        try:
+            response = await self.__client_session.get(url, headers=headers)
+            return_response = await _httpx_response_handler(response)
+        except Exception as ex:
+            return_response = HTTPResponse(text=str(ex), status_code=999)
+        return return_response
+
+    async def post(self, url: str, data: any, headers: dict | None = None):
+        try:
+            response = await self.__client_session.post(url, data=data, headers=headers)
+            return_response = await _httpx_response_handler(response)
+        except Exception as ex:
+            return_response = HTTPResponse(text=str(ex), status_code=999)
+        return return_response
+
+    async def close(self) -> None:
+        await self.__client_session.aclose()
+        await asyncio.sleep(0.250)
+
+    def __create_client(self) -> httpx.AsyncClient:
+        return httpx.AsyncClient(proxy=self.__proxy, verify=self.__ssl_strict)
+
+
+async def _httpx_response_handler(response: httpx.Response) -> HTTPResponse:
+    status_code: int = 999
+    return_response: HTTPResponse
+
+    try:
+        status_code = response.status_code
+        response.raise_for_status()
+        return_response = HTTPResponse(text=response.text, status_code=status_code)
+        await response.aclose()
+    except httpx.HTTPStatusError as status_error:
+        return_response = HTTPResponse(text=str(status_error), status_code=status_code)
+    except Exception as ex:
+        return_response = HTTPResponse(text=str(ex), status_code=status_code)
+
+    return return_response
---
+++ nebx/interfaces/IRequestWrapper.py
@@ -0,0 +1,17 @@
+﻿from abc import ABC, abstractmethod
+
+from nebx.types import HTTPResponse
+
+
+class IRequestWrapper(ABC):
+    @abstractmethod
+    async def get(self, url: str, headers: dict | None = None) -> HTTPResponse:
+        pass
+
+    @abstractmethod
+    async def post(self, url: str, data: any, headers: dict | None = None) -> HTTPResponse:
+        pass
+
+    @abstractmethod
+    async def close(self) -> None:
+        pass
