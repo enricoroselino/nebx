4c9dbb62adae4e2d06929c6a21805e157755fbec
enricoroselino <enricoroselino@gmail.com>
2024-05-07 09:52:13 +0700

    feat(code): added ScheduleManager.py to manage simple scheduling

---
+++ nebx/core/ScheduleManager.py
@@ -0,0 +1,21 @@
+﻿import schedule
+import math
+
+
+class ScheduleManager:
+    @staticmethod
+    def get_schedules() -> list[schedule.Job]:
+        return schedule.get_jobs()
+
+    @staticmethod
+    def add_next_job(countdown_seconds: float | int, job: any, *job_tags, **job_params) -> None:
+        converted_int: int = math.ceil(countdown_seconds) if isinstance(countdown_seconds, float) else countdown_seconds
+        schedule.every(converted_int).seconds.do(job, **job_params).tag(*job_tags)
+
+    @staticmethod
+    def clear_jobs(*tags):
+        schedule.clear(*tags)
+
+    @staticmethod
+    def run_jobs(immediate: bool = False) -> None:
+        schedule.run_all() if immediate is True else schedule.run_pending()
