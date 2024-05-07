6ca1dda4ab214f57151f8b1a62d593edde9c33e5
enricoroselino <enricoroselino@gmail.com>
2024-05-07 09:47:54 +0700

    feat(code): added ConcurrentTaskManager and IConcurrentTaskManager
    added ConcurrentTaskManager to manage tasks that gonna run concurrently

---
+++ nebx/core/ConcurrentTaskManager.py
@@ -0,0 +1,18 @@
+﻿import asyncio
+from typing import Self
+
+from nebx.interfaces.IConcurrentTaskManager import IConcurrentTaskManager
+
+
+class ConcurrentTaskManager(IConcurrentTaskManager):
+    def __init__(self):
+        self.__tasks = []
+
+    def add_task(self, task: asyncio.Task) -> Self:
+        self.__tasks.append(task)
+        return self
+
+    async def run_tasks(self) -> tuple[any]:
+        result = await asyncio.gather(*self.__tasks)
+        self.__tasks.clear()
+        return result
---
+++ nebx/interfaces/IConcurrentTaskManager.py
@@ -0,0 +1,13 @@
+﻿from abc import ABC, abstractmethod
+import asyncio
+from typing import Self
+
+
+class IConcurrentTaskManager(ABC):
+    @abstractmethod
+    def add_task(self, task: asyncio.Task) -> Self:
+        pass
+
+    @abstractmethod
+    async def run_tasks(self) -> tuple[any]:
+        pass
