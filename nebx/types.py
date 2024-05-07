04553eaa15148a0e3770230b8dfd8296c03c6c70
enricoroselino <enricoroselino@gmail.com>
2024-05-07 09:50:41 +0700

    feat(code): added HTTPResponse and EmailSetting
    - added HTTPResponse dataclass to substitute the API Response that need to be closed
    - added EmailSetting dataclass to encapsulate the email credentials

---
+++ nebx/types.py
@@ -0,0 +1,19 @@
+﻿from dataclasses import dataclass
+from json import loads
+
+
+@dataclass
+class HTTPResponse:
+    text: str
+    status_code: int
+
+    def json(self):
+        return loads(self.text) if self.text is not None else {}
+
+
+@dataclass
+class MailerSetting:
+    smtp_server: str
+    username: str
+    password: str
+    port: int
