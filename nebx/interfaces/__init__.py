2feb8a6416f0439e135b064c6a39917ff57df70c
enricoroselino <enricoroselino@gmail.com>
2024-05-07 10:05:37 +0700

    feat(code): added __init__.py in folders
    added __init__.py in folders to refer the implemented codes for nebx

---
+++ nebx/interfaces/__init__.py
@@ -0,0 +1,4 @@
+﻿from nebx.interfaces.IRequestWrapper import IRequestWrapper
+from nebx.interfaces.IHttpHeaderFaker import IHttpHeaderFaker
+from nebx.interfaces.IConcurrentTaskManager import IConcurrentTaskManager
+from nebx.interfaces.IMailer import IMailer
