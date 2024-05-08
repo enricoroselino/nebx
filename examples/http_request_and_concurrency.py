4c62cfa5735491b4bb1e867ed52825ed70557c58
enricoroselino <enricoroselino@gmail.com>
2024-05-08 21:45:45 +0700

    docs(example): add code example for http request and concurrency

---
+++ examples/http_request_and_concurrency.py
@@ -0,0 +1,27 @@
+﻿import asyncio
+from time import perf_counter
+
+from nebx import RequestWrapperHttpx, HttpHeaderFaker, ConcurrentTaskManager
+from nebx.interfaces import IRequestWrapper, IHttpHeaderFaker, IConcurrentTaskManager
+
+
+async def main():
+    task_manager: IConcurrentTaskManager = ConcurrentTaskManager()
+    header_faker: IHttpHeaderFaker = HttpHeaderFaker()
+    request: IRequestWrapper = RequestWrapperHttpx()
+
+    start = perf_counter()
+    for i in range(1, 100):
+        header_faker.change_user_agent()
+        coro = request.get(f"https://dummyjson.com/products/{i}", headers=header_faker.header)
+        task = asyncio.create_task(coro)
+        task_manager.add_task(task)
+
+    responses = await task_manager.run_tasks()
+    stop = perf_counter()
+    print(stop - start, responses)
+    await request.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
